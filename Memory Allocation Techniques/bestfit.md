af46cb5bdf93bf9b31e29b12f11e519618a19652
RetroJoshua <joshua537.nit@gmail.com>
2025-12-08 10:30:43 +0530

    modified:   Memory Allocation Techniques/bestfit.md 	modified:   Memory Allocation Techniques/worestfit.c 	new file:   page replacement algorithms/LRU.c

--- Memory Allocation Techniques/bestfit.md
+++ Memory Allocation Techniques/bestfit.md
@@ -1,11 +1,12 @@
+Develop a C program to simulate the following contiguous memory allocation Techniques: 
+a) Best fit b) First fit c) Worest fit. 
 ## Concept:
 One of the simplest methods for memory allocation is to divide memory into several fixed-sized partitions. Each partition may contain exactly one process. In this multiple-partition method, when a partition is free, a process is selected from the input queue and is loaded into the free partition. When the process terminates, the partition becomes available for another process. The operating system keeps a table indicating which parts of memory are available and which are occupied. Finally, when a process arrives and needs memory, a memory section large enough for this process is provided. When it is time to load or swap a process into main memory, and if there is more than one free block of memory of sufficient size, then the operating system must decide which free block to allocate. Best-fit strategy chooses the block that is closest in size to the request.
 
 ## Algorithm:
 - Step 1- Input memory blocks and processes with sizes. 
 - Step 2- Initialize all memory blocks as free. 
-- Step 3- Start by picking each process and find the minimum block size that can be  
-assigned to  current process i.e.,  find min(bockSize[1],    blockSize[2],.....blockSize[n]) >   processSize[current], 
+- Step 3- Start by picking each process and find the minimum block size that can be  assigned to  current process i.e.,  find min(bockSize[1],    blockSize[2],.....blockSize[n]) >   processSize[current], 
 - Step 4: if found then assign    it to the current process. 
 - 5- If not then leave that process and keep checking    the further processes. 
 
@@ -23,6 +24,6 @@ Enter the size of the files:-
 File 1: 1 
 File 2: 4 
  
-File No                  File Size               Block No             Block Size           Fragment 
+File No     File Size               Block No             Block Size           Fragment 
 1                              1                             2                             2                             1 
 2                              4                              1                              5                             1
--- Memory Allocation Techniques/worestfit.c
+++ Memory Allocation Techniques/worestfit.c
@@ -1,64 +1,75 @@
-#include <stdio.h>
-#define max 25
+#include<stdio.h> 
+#define max 25 
 
-void main()
-{
-    int frag[max], b[max], f[max], i, j, nb, nf, temp;
-    static int bf[max], ff[max];
-    
-    printf("\n\tMemory Management Scheme - First Fit");
-    printf("\nEnter the number of blocks:");
-    scanf("%d", &nb);
-    printf("Enter the number of files:");
-    scanf("%d", &nf);
-    
+int main() 
+{ 
+    int frag[max],b[max],f[max],i,j,nb,nf,temp,highest; 
+    static int bf[max],ff[max]; 
+
+    printf("\n\tMemory Management Scheme - Worst Fit"); 
+    printf("\nEnter the number of blocks:"); 
+    scanf("%d",&nb); 
+    printf("Enter the number of files:"); 
+    scanf("%d",&nf); 
+
     printf("\nEnter the size of the blocks:-\n");
-    for (i = 1; i <= nb; i++)
-    {
-        printf("Block %d:", i);
-        scanf("%d", &b[i]);
-    }
-    
-    printf("Enter the size of the files :-\n");
-    for (i = 1; i <= nf; i++)
-    {
-        printf("File %d:", i);
-        scanf("%d", &f[i]);
-    }
-    
-    for (i = 1; i <= nf; i++)
-    {
-        temp = -1;  // Initialize temp to indicate no allocation
-        for (j = 1; j <= nb; j++)
-        {
-            if (bf[j] != 1)
-            {
-                temp = b[j] - f[i];
-                if (temp >= 0)
+    for(i=0;i<nb;i++) // Changed loop to 0-indexed for consistency
+    { 
+        printf("Block %d:",i+1); // Displaying 1-indexed for user
+        scanf("%d",&b[i]); 
+    } 
+
+    printf("Enter the size of the files :-\n"); 
+    for(i=0;i<nf;i++) // Changed loop to 0-indexed for consistency
+    { 
+        printf("File %d:",i+1); // Displaying 1-indexed for user
+        scanf("%d",&f[i]); 
+    } 
+
+    // Initialize bf and ff arrays
+    for(i=0; i<nb; i++) bf[i] = 0;
+    for(i=0; i<nf; i++) ff[i] = -1; // Initialize with -1 to indicate no block assigned
+
+    for(i=0;i<nf;i++) // Loop through files (0-indexed)
+    { 
+        highest=-1; // Reset highest for each file
+        int worst_fit_block_idx = -1; // To store the index of the worst fit block
+
+        for(j=0;j<nb;j++) // Loop through blocks (0-indexed)
+        { 
+            if(bf[j]!=1) // If block is not allocated
+            { 
+                temp=b[j]-f[i]; 
+                if(temp>=0) // If block is large enough
                 {
-                    ff[i] = j;
-                    frag[i] = temp;
-                    bf[j] = 1;
-                    break;
-                }
-            }
-        }
-        
-        // If no block was found, mark it
-        if (temp < 0)
+                    if(temp > highest) // Find the block with the largest remaining space
+                    {
+                        highest=temp;
+                        worst_fit_block_idx = j; // Store the index of this block
+                    }
+                } 
+            } 
+        } 
+
+        if(worst_fit_block_idx != -1) // If a suitable block was found
         {
-            ff[i] = -1;
-            frag[i] = -1;
+            ff[i]=worst_fit_block_idx; // Assign the block index to the file
+            frag[i]=highest; // Store the fragmentation
+            bf[worst_fit_block_idx]=1; // Mark the block as allocated
+        } else {
+            frag[i] = -1; // Indicate that the file could not be allocated
         }
-    }
-    
-    printf("\nFile_no:\tFile_size:\tBlock_no:\tBlock_size:\tFragment");
-    for (i = 1; i <= nf; i++)
-    {
-        if (ff[i] != -1)
-            printf("\n%d\t\t%d\t\t%d\t\t%d\t\t%d", i, f[i], ff[i], b[ff[i]], frag[i]);
-        else
-            printf("\n%d\t\t%d\t\tNot Allocated\t-\t\t-", i, f[i]);
-    }
-    printf("\n");
+    } 
+
+    printf("\nFile_no:\tFile_size :\tBlock_no:\tBlock_size:\tFragment"); 
+    for(i=0;i<nf;i++) // Loop through files (0-indexed)
+    { 
+        if(ff[i] != -1) { // If the file was allocated
+            printf("\n%d\t\t%d\t\t%d\t\t%d\t\t%d",i+1,f[i],ff[i]+1,b[ff[i]],frag[i]); 
+        } else {
+            printf("\n%d\t\t%d\t\tNot Allocated\t\t--\t\t--",i+1,f[i]);
+        }
+    } 
+    printf("\n"); // Add a newline at the end for better formatting
+    return 0; // Standard practice for int main()
 }
